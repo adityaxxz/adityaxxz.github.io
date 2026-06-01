6423c4aa640724952a1bb91351c916d6d2adc134
adityaxxz <adityaranjan5995@gmail.com>
2026-06-01 23:11:55 +0530

    resume update

--- themes/blowfish/assets/js/search.js
+++ themes/blowfish/assets/js/search.js
@@ -187,7 +187,7 @@ function executeQuery(term) {
         resultsHTML +
         `<li class="mb-2">
           <a class="flex items-center px-3 py-2 rounded-md appearance-none bg-neutral-100 dark:bg-neutral-700 focus:bg-primary-100 hover:bg-primary-100 dark:hover:bg-primary-900 dark:focus:bg-primary-900 focus:outline-dotted focus:outline-transparent focus:outline-2" 
-          href="https://drive.google.com/file/d/1cDKVOwWSumPp72rOrM1u7JylFHP26ni5/view" target="_blank" rel="noopener" tabindex="0">
+          href="https://drive.google.com/file/d/1btQq0sHctDie809GwqVgT1tv1X2LOKKq/view" target="_blank" rel="noopener" tabindex="0">
             <div class="grow">
               <div class="-mb-1 text-lg font-bold">
                 Resume
